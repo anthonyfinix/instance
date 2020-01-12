7540f0dfba968c504d7610b0ea7269dbf9293ea6
anthonyfinix <anthonyfinix@gmail.com>
2020-01-12 17:27:30 +0530

    before client

--- src/app/class/view/class-view.component.ts
+++ src/app/class/view/class-view.component.ts
@@ -16,60 +16,41 @@ export class ClassViewComponent implements OnInit, OnDestroy {
   displayedColumns: string[] = [];
   collection$: Observable<unknown[]>;
   paramSub: Subscription;
+  collectionSub: Subscription;
 
   constructor(
     private actRoute: ActivatedRoute,
     private afs: AngularFirestore
   ) {
-
     this.displayedColumns = [];
     this.ELEMENT_DATA = [];
-    // this.classCheck = this.afs.collection(`/classes`).doc(`${this.getCurrentParam()}`).get().subscribe(doc=>{
-    //   if(doc.exists){
-    //     this.collection$ = this.afs.collection(`/${this.getCurrentParam()}`).valueChanges();
-    //     this.title = this.getCurrentParam();
-
-    //   }else{
-    //     this.title = 'sorry'
-    //   }
-    // })
-    // this.actRoute.paramMap.subscribe(data=>{
-    //   console.log(data.get('class'))
-    // })
-    this.actRoute.params.pipe(
-      tap(data => {
-        console.log(data)
-      })
-    )
-
   }
 
   ngOnInit() {
-    // this.afs.collection(`/classes`).doc(`${this.getCurrentParam()}`).get().subscribe(doc=>{
-    //   console.log(doc.id)
-    // })
-    // this.paramSub = this.actRoute.paramMap.subscribe(data => {
-    //   // this.title = data.get('class');
-    //   const docRef = this.afs.collection(`classes`).doc(`${data.get('class')}`);
-    //   docRef.get().subscribe(doc=>{
-    //     if (doc.exists) {
-    //       this.title = data.get('class')
-    //     }else{
-    //       this.title = 'not a title'
-    //     }
-    //   })
-    //   this.collection$ = this.afs.collection(`/${this.title}`).valueChanges();
-    //   this.collection$.subscribe(data=>{
-    //     this.ELEMENT_DATA = data;
-    //     let fields = []
-    //     data.forEach(obj=>{
-    //       fields = Object.keys(obj)
-    //     })
-    //     this.displayedColumns = [... new Set(fields)]
-    //     this.dataSource = this.ELEMENT_DATA;
-    //   })
-    // })
+
+    this.paramSub = this.actRoute.paramMap.subscribe(params => {
+      this.afs.collection(`/classes`).doc(`${params.get('class')}`).get().subscribe(doc => {
+        if (doc.exists) {
+          this.collection$ = this.afs.collection(`/${params.get('class')}`).valueChanges();
+          this.title = params.get('class');
+          this.collectionSub = this.collection$.subscribe(data => {
+            this.ELEMENT_DATA = data;
+            let fields = []
+            data.forEach(obj => {
+              fields = Object.keys(obj)
+            })
+            this.displayedColumns = [... new Set(fields)]
+            this.dataSource = this.ELEMENT_DATA;
+          })
+
+        } else {
+          this.title = 'sorry'
+        }
+      })
+    })
   }
   ngOnDestroy() {
+    this.paramSub.unsubscribe()
+    this.collectionSub.unsubscribe()
   }
 }
